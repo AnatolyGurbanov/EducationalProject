96e82d6217d5cb9a74d8e96d2b5bc3f9bfa78e85
Anatoly Gurbanov <tomgurbanov@gmail.com>
2021-03-16 18:28:23 +0300

    add network layer as target/framework

---
+++ Networking/Config/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Networking
+//
+//  Created by Anatoly Gurbanov on 16.03.2021.
+//
+
+import Foundation
+
+enum Constants {
+    static let apiPrivateKey: String = "03da6d84-de02-4d7f-babf-1358bfcbfd8c"
+}
